ec7f0b3fb6b29d4a1a7bb5e4d7e376475e5ded71
Pamela Rodrigues <akahime2014@gmail.com>
2025-06-17 00:05:45 -0300

    SistemaHotel

---
+++ SistemaHotel/SistemaHotel/Program.cs
@@ -0,0 +1,41 @@
+﻿void formatter()
+{
+    for (int index = 0; index < 25; index++)
+    {
+        Console.Write("==");
+    }
+    
+}
+
+void titulo(string nome)
+{
+    Console.Write("\n======           " + nome + "          ======\n");
+}
+
+void coracao()
+{ 
+    Console.WriteLine(" ");
+    Console.Write(" ");
+    for (int linhas = 6; linhas >= 3; linhas--)
+    { 
+        Console.Write("&&");
+    }
+    Console.WriteLine(" ");
+    for (int linhas = 6; linhas > 0; linhas--)
+    {
+        Console.Write(" ");
+        for (int caracteres = linhas; caracteres > 0; caracteres--)
+        {
+            Console.Write("&&");
+        
+        }
+        Console.WriteLine(" ");
+    }
+    
+}
+
+formatter();
+titulo("HOTEL DE ACAPULCO");
+formatter();
+coracao();
+
